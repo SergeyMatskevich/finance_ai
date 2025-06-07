86dec220e386ca56907d8b11f3faa51519633971
Sergey Matskevich <gingerzlo@yandex.ru>
2025-06-08 00:05:39 +0200

    Add POSTGRES_PORT to Settings

--- app/core/config.py
+++ app/core/config.py
@@ -6,16 +6,17 @@ class Settings(BaseSettings):
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     
-    # Security
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    
-    # Database
+    # Database settings
     POSTGRES_SERVER: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    POSTGRES_PORT: int = 5432
+    
+    # Security
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
     model_config = SettingsConfigDict(case_sensitive=True)
 
